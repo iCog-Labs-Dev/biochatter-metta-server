3cbc6053b66f55e41875e277f5e1cd058da4a1a3
Tollan Sitotaw <tollanberhanu@gmail.com>
2024-05-20 16:19:56 +0300

    fix: reverse chat list

--- api/views.py
+++ api/views.py
@@ -15,7 +15,7 @@ from biochatter_metta.metta_prompt import get_schema_items
 
 class ChatList(APIView):
     def get(self, request):
-        chats = Chat.objects.all().reverse()
+        chats = Chat.objects.all().order_by('-chat_created_at')
         return get_paginated_records(
             pagination_class=LimitOffsetPagination,
             request=request,
